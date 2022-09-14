a836e21e12a59476bd342f9709c443fbe58a8cda
Jakub Godlewski <jakub.godlewski@yahoo.com>
2022-09-14 15:51:56 +0200

    create handleZeroClick function

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,8 +1,60 @@
 import "./Board.css"
+import {useCheckAround} from "../hooks/useCheckAround";
 
-const Board = ({board}) => {
+const Board = ({board, updateBoard}) => {
+    const [checkAround] = useCheckAround()
 
 
+
+        //when user clicks 0, all 0 that touch that element should be uncovered.
+        // also all zeros that touch any of these 0 should be uncovered etc.
+    const handleZeroClick = (elId) => {
+
+        let zerosAroundIds = []
+
+        zerosAroundIds.push(elId)
+
+        const loopZerosIds = () => {
+            let extraElements = false
+
+            zerosAroundIds.forEach((id)=>{
+                let elements = checkAround(id, board)
+                elements.forEach((element)=>{
+                    if (element.number === 0 && !zerosAroundIds.includes(element.id)){
+                        zerosAroundIds.push(element.id)
+                        extraElements = true
+                    }
+                })
+            })
+            if (extraElements){
+                console.log("infi ):")
+                loopZerosIds()
+            }
+        }
+
+        loopZerosIds()
+
+        //now uncover (change clicked key to true) all numbers !==0 which are next to any 0
+        const allNumbersAround = [...zerosAroundIds]
+
+        zerosAroundIds.forEach((id)=>{
+            let elements = checkAround(id, board)
+            elements.forEach((el)=>{
+                if (!zerosAroundIds.includes(el.id)){
+                    allNumbersAround.push(el.id)
+                }
+            })
+        })
+
+
+        let updatedBoard = board.map((element)=> {
+            if (zerosAroundIds.includes(element.id) || allNumbersAround.includes(element.id)){
+                return {...element, clicked: true}
+            }else return element
+        })
+
+        return updatedBoard
+    }
 
     const handleElementClick = (el) => {
         let updatedBoard;
@@ -23,7 +75,8 @@ const Board = ({board}) => {
             if (el.mine || el.number !==0){
                 //INSERT FUNCTION - reset game
             }else {
-                // INSERT FUNCTION - handle zero click
+                let updatedBoardWithZeros = handleZeroClick(el.id)
+                updatedBoard = updatedBoardWithZeros
             }
 
             // if not first click
@@ -31,16 +84,25 @@ const Board = ({board}) => {
             if (el.mine){
                 //INSERT FUNCTION - game lost
             }else if (el.number === 0){
-                // INSERT FUNCTION - handle zero click
+                let updatedBoardWithZeros = handleZeroClick(el.id)
+                updatedBoard = updatedBoardWithZeros
             }
         }
+
+
+        //update main board
+        console.log(updatedBoard)
+        updateBoard(updatedBoard)
+
     }
 
 
     return (
         <div className="board">
             {board.map((element)=>(
-                <div onClick={()=>handleElementClick(element)} className="element" key={element.id}></div>
+                <div onClick={()=>handleElementClick(element)} className={`element ${element.clicked ? "clicked" : ""}`} key={element.id}>
+                    {element.clicked && !element.mine && element.number !== 0 && element.number}
+                </div>
             ))}
         </div>
     );
