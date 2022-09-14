381c8f70f9c3af4dfe541cc1738b94511436b0e3
Jakub Godlewski <jakub.godlewski@yahoo.com>
2022-09-14 18:12:33 +0200

    add functon which resets game if first click was not equal 0 - thanks to that every first click is always 0

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,14 +1,13 @@
 import "./Board.css"
 import {useCheckAround} from "../hooks/useCheckAround";
 
-const Board = ({board, updateBoard, updateGameStatus}) => {
+const Board = ({board, updateBoard, updateGameStatus, initializeGame}) => {
     const [checkAround] = useCheckAround()
-
 
 
         //when user clicks 0, all 0 that touch that element should be uncovered.
         // also all zeros that touch any of these 0 should be uncovered etc.
-    const handleZeroClick = (elId) => {
+    const handleZeroClick = (elId, board) => {
 
         let zerosAroundIds = []
 
@@ -61,14 +60,42 @@ const Board = ({board, updateBoard, updateGameStatus}) => {
     const handleRightClick = (e, el) => {
         e.preventDefault()
         const updatedBoard = board.map((element)=>{
-            if (el.id===element.id){
-                if (element.flagged){
-                    return {...element, flagged: false}
-                } else if (!element.flagged){
-                    return {...element, flagged: true}
-                }
-            }else return element
+            if (!el.clicked){
+                if (el.id===element.id){
+                    if (element.flagged){
+                        return {...element, flagged: false}
+                    } else if (!element.flagged){
+                        return {...element, flagged: true}
+                    }
+                }else return element
+            }
+
         })
+
+        //check if user won game
+
+        let flagged = []
+        let mines = []
+        updatedBoard.forEach((el)=>{
+            if (el.mine){
+                mines.push(el.id)
+            }
+            if (el.flagged){
+                flagged.push(el.id)
+            }
+        })
+        if (flagged.length===mines.length){
+            let i = 0
+            flagged.forEach((id)=>{
+                if (mines.includes(id)){
+                    i++
+                }
+            })
+            if (i===10){
+                updateGameStatus("WON")
+            }
+        }
+
         updateBoard(updatedBoard)
     }
 
@@ -87,12 +114,27 @@ const Board = ({board, updateBoard, updateGameStatus}) => {
                     return {...element, clicked: true}
                 } else return  element
         })
+
               //if first click
         if (firstClick){
             if (el.mine || el.number !==0){
-                //INSERT FUNCTION - reset game
+                    let board = initializeGame()
+                    while(board[el.id].number!==0){
+                        board = initializeGame()
+                    }
+
+                    updatedBoard = board.map((element)=>{
+                        if (el.id === element.id){
+                            return {...element, clicked: true}
+                        }else return  element
+                    })
+
+                let updatedBoardWithZeros = handleZeroClick(el.id, updatedBoard)
+                updatedBoard = updatedBoardWithZeros
+
+
             }else {
-                let updatedBoardWithZeros = handleZeroClick(el.id)
+                let updatedBoardWithZeros = handleZeroClick(el.id, board)
                 updatedBoard = updatedBoardWithZeros
             }
 
@@ -101,15 +143,13 @@ const Board = ({board, updateBoard, updateGameStatus}) => {
             if (el.mine){
                 updateGameStatus("LOST")
             }else if (el.number === 0){
-                let updatedBoardWithZeros = handleZeroClick(el.id)
+                let updatedBoardWithZeros = handleZeroClick(el.id, board)
                 updatedBoard = updatedBoardWithZeros
             }
         }
 
-
         //update main board
         updateBoard(updatedBoard)
-
     }
 
 
