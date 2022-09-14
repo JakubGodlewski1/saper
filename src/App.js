c50fde9b47bbc4630f9ea4ae557d70c692af020a
Jakub Godlewski <jakub.godlewski@yahoo.com>
2022-09-14 13:13:26 +0200

    create game Initialization function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,54 @@
 import './App.css';
+import {useCheckAround} from "./hooks/useCheckAround";
+import {useState} from "react";
 
 function App() {
+  const [checkAround] = useCheckAround()
+  const [board, setBoard] = useState([])
+
+
+
+  const initializeGame = () => {
+    //create 10 mines - 10 random numbers between 0 and 99. Later we will place them into the board
+    const mines = []
+    while (mines.length < 10){
+      let mine = Math.floor(Math.random()*100)
+      if (!mines.includes(mine)){
+        mines.push(mine)
+      }
+    }
+
+    //create a board - 100 objects
+    const board = []
+    for (let i = 0; i < 100; i++) {
+      board.push({
+        id: i,
+        clicked: false,
+        flagged: false,
+        mine: mines.includes(i),
+        number: null
+      })
+    }
+
+    //place appropriate number to every field. every number has to correspond to amount of mines that are around the field.
+    let boardWithNumbers = board.map((element)=>{
+      let numberOfMinesAround = 0;
+
+      let elementsAround = checkAround(element.id,  board)
+      elementsAround.forEach((element)=>{
+        if (element.mine){
+          numberOfMinesAround++
+        }
+      })
+      return {...element, number: numberOfMinesAround}
+    })
+
+   setBoard(boardWithNumbers)
+   return boardWithNumbers
+  }
+
+  initializeGame()
+
   return (
     <div className="App">
 
